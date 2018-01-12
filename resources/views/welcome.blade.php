02f7721c4c7a4cce1360328a19bc1b608dec8e75
Tonatiuh Flores Castelán <tonatiuh@elmamut.com>
2018-01-12 10:30:09 -0600

    fix path to css and js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,13 +10,13 @@
             [v-cloak] { display: none; }
             #app { margin-top: 40px; }
         </style>
-        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <body>
         <div id="app">
             <todo-component></todo-component>
         </div>
-        <script src="{{ mix('/js/app.js') }}"></script>
+        <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
